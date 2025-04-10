e931ac9db8667c114a2e1664ed99fc8309c29c43
Olha Mykytka <olhamykytka00@gmail.com>
2025-04-10 13:23:30 -0600

    05

---
+++ js/ratings.js
@@ -0,0 +1,22 @@
+export const reviews = [
+    {
+        "name" : "Sully",
+        "stars" : 5,
+        "text" : "The sports car auto sales place offers a wide selection of high-performance vehicles for enthusiasts."
+    },
+    {
+        "name" : "Mike",
+        "stars" : 1,
+        "text" : "All the counters in this place are too short, I couldn't see over any of them."
+    },
+    {
+        "name" : "Dash",
+        "stars" : 3,
+        "text" : "The cars look cool, but I feel like I can run faster than all of them. Except for the Corolla that is."
+    },
+    {
+        "name" : "Violet",
+        "stars" : 4,
+        "text" : "They told me I had to leave because I kept making myself invisible to hide from the bad guys. Cheap cars though."
+    },
+]
